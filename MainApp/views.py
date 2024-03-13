548e7b4c5f8923bb2e1c86fe24cb9a52977ef6d3
raisonCollab <danil.zinovev.16@mail.ru>
2024-03-13 18:28:40 +0300

    errors

--- MainApp/views.py
+++ MainApp/views.py
@@ -1,9 +1,11 @@
+from pprint import pprint
 from typing import Type
 
 from django.core.handlers.wsgi import WSGIRequest
 from django.forms import forms
 from django.http import HttpResponse, HttpRequest
 from django.shortcuts import render, get_object_or_404, redirect
+from django.views.decorators.csrf import csrf_exempt
 
 from .forms import create_form
 from .models import Form
@@ -14,6 +16,7 @@ def index(request: WSGIRequest) -> HttpResponse:
     return HttpResponse("Hello")
 
 
+@csrf_exempt
 def form_view(request: HttpRequest, form_id: int) -> HttpResponse:
 
     form_model: Form = get_object_or_404(Form, id=form_id)
@@ -23,8 +26,7 @@ def form_view(request: HttpRequest, form_id: int) -> HttpResponse:
     if request.method == 'POST':
         form: forms.Form = form_class(request.POST)
 
-        if form.is_valid():
-            return redirect('success')
+        pprint(form.errors)
 
     else:
         form: forms.Form = form_class()
